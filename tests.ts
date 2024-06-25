a47cfc2ba23bff2991748b2f5df6777803d83845
Raphael Darley <raphael@raphaeldarley.com>
2024-06-25 18:58:48 +0100

    fix tests

--- tests.ts
+++ tests.ts
@@ -5,12 +5,18 @@ const surreal = new Surreal({
     engines: surrealdbNodeEngines()
 });
 
-// console.log("connecting", await surreal.connect("mem://"));
+async function run() {
+    console.log("connecting", await surreal.connect("mem://"));
 
-// console.log("using", await surreal.use({ namespace: "test", database: "test" }));
+    console.log("using", await surreal.use({ namespace: "test", database: "test" }));
 
-// console.log("listening", await surreal.live("test", (res) => console.log("recieved live" + res)));
+    console.log("listening", await surreal.live("test", (res) => console.log("recieved live" + res)));
 
-// console.log("creating", await surreal.create('test', { val: 42 }));
+    console.log("creating", await surreal.create('test', { val: 42 }));
 
-// console.log("closing", await surreal.close());
+    console.log("selecting", await surreal.select('test'));
+
+    console.log("closing", await surreal.close());
+}
+
+run()
