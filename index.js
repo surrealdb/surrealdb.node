c90b01721dd03ec73c3dbead9577d97e3aab211d
Raphael Darley <raphael@raphaeldarley.com>
2024-06-26 12:23:40 +0100

    fix build cmd

--- index.js
+++ index.js
@@ -224,72 +224,14 @@ switch (platform) {
         }
         break
       case 'arm':
-        if (isMusl()) {
-          localFileExisted = existsSync(
-            join(__dirname, 'surrealdb.node.linux-arm-musleabihf.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./surrealdb.node.linux-arm-musleabihf.node')
-            } else {
-              nativeBinding = require('surrealdb.node-linux-arm-musleabihf')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        } else {
-          localFileExisted = existsSync(
-            join(__dirname, 'surrealdb.node.linux-arm-gnueabihf.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./surrealdb.node.linux-arm-gnueabihf.node')
-            } else {
-              nativeBinding = require('surrealdb.node-linux-arm-gnueabihf')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        }
-        break
-      case 'riscv64':
-        if (isMusl()) {
-          localFileExisted = existsSync(
-            join(__dirname, 'surrealdb.node.linux-riscv64-musl.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./surrealdb.node.linux-riscv64-musl.node')
-            } else {
-              nativeBinding = require('surrealdb.node-linux-riscv64-musl')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        } else {
-          localFileExisted = existsSync(
-            join(__dirname, 'surrealdb.node.linux-riscv64-gnu.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./surrealdb.node.linux-riscv64-gnu.node')
-            } else {
-              nativeBinding = require('surrealdb.node-linux-riscv64-gnu')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        }
-        break
-      case 's390x':
         localFileExisted = existsSync(
-          join(__dirname, 'surrealdb.node.linux-s390x-gnu.node')
+          join(__dirname, 'surrealdb.node.linux-arm-gnueabihf.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./surrealdb.node.linux-s390x-gnu.node')
+            nativeBinding = require('./surrealdb.node.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('surrealdb.node-linux-s390x-gnu')
+            nativeBinding = require('surrealdb.node-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
